48bfb238d05758b9a0e1d8c917d20f796a25fceb
victoriak94 <vkamila6@gmail.com>
2018-12-18 14:02:40 -0500

    Add csv and json formats. Edit index view for html and text formats. Complete assignment

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,20 @@ class BooksController < ApplicationController
       respond_to do |format|
       format.html
       format.text
+
+      format.csv do
+        render plain: Book.generate_csv(@books)
+      end
+
+      format.json do
+        render json: @books.map do |book|
+          {
+            title: book.title,
+            author: book.author,
+            already_read: book.already_read
+          }
+        end
+      end
     end
   end
-
 end
